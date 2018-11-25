fd93656991ddbb67d233972d72187744c5b4fc8e
Aaron Christianson <ninjaaron@gmail.com>
2018-11-25 20:45:53 +0100

    finished the async version

--- slowpool/aio.py
+++ slowpool/aio.py
@@ -1,4 +1,5 @@
 from libaaron import aio
+import typing as t
 
 
 class NoCompleteTasks(Exception):
@@ -6,46 +7,74 @@ class NoCompleteTasks(Exception):
 
 
 class Pool:
-    """a async interface that mimics a slowpool.Pool"""
+    """a async interface that mimics a slowpool.Pool -- to some extent"""
 
     def __init__(self, maxworkers=1):
+        """create a new blocking task pool.
+        
+        - maxworkers: maximum number simultanious tasks.
+        """
         self.max = maxworkers
         self.tasks = set()
 
     async def submit(self, coro):
+        """add a new coroutine to the pool. block while the maximum
+        number of jobs is still running.
+        """
         if len(self.tasks) >= self.max:
             _, self.tasks = await aio.wait(self.tasks, return_when=aio.FIRST_COMPLETED)
         task = aio.spawn(coro)
         self.tasks.add(task)
         return task
 
-    def _map_queue(self, fn, iterable):
+    def _map_tasks(self, fn, iterable, taskcallback):
+        """returns a task that will submit new tasks to the pool by
+        mapping function to iterable.
+
+        - fn: coroutine function.
+        - iterable: iterable that gets mapped to the function
+        - taskcallback: after each task is created taskcallback is called
+          on it. taskcallback is an async function. Normally used for 
+          sending the task somewhere interesting, like into a queue.
+        """
         iterator = iter(iterable)
-        q = aio.Queue()
 
         async def queueall():
             while True:
                 try:
                     task = await self.submit(fn(next(iterator)))
-                    await q.put(task)
+                    await taskcallback(task)
                 except StopIteration:
-                    await q.put(None)
                     return
 
-        aio.spawn(queueall())
-        return q
+        return aio.spawn(queueall())
 
-            
-
-    async def map(self, fn, iterable):
-        tasks = [await self.submit(fn(item)) for item in iterable]
-        return await aio.gather(*tasks)
+    def map(self, fn, iterable):
+        """map the coroutine function on to the iterable, submitting
+        them as tasks too the pool.
+        
+        returns an AsyncIterator. Results appear as they become available, but
+        in the original order.
+        """
+        q = aio.Queue()
+        return SyncMap(self._map_tasks(fn, iterable, q.put), q)
 
     def amap(self, fn, iterable):
+        """map the coroutine function on to the iterable, submitting
+        them as tasks too the pool.
         
-        return AsyncMap(self._map_queue(fn, iterable))
+        returns an AsyncIterator. Results appear in the order the finish, not
+        in the order they were submitted.
+        """
+        tasks = set()
+
+        async def add(task):
+            tasks.add(task)
+
+        return AsyncMap(self._map_tasks(fn, iterable, add), tasks)
 
     async def empty(self):
+        """wait for all tasks to complete"""
         try:
             _, self.tasks = await aio.wait(self.tasks)
         except ValueError:
@@ -56,32 +85,37 @@ class Pool:
 
     async def __aexit__(self, exc_type, exc_value, traceback):
         await self.empty()
-        
 
-async def next_complete(tasks):
-    if not tasks:
-        raise StopAsyncIteration
-    for i, task in enumerate(tasks):
-        await aio.sleep(0)
-        if task.done():
-            del tasks[i]
-            return await task
-    raise NoCompleteTasks
+
+class SyncMap:
+    """Return type of Pool.map"""
+
+    def __init__(self, queuer: aio.Task, q: aio.Queue):
+        """queuer is the task which puts submitted tasks in the q"""
+        self.queuer = queuer
+        self.q = q
+
+    def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        while True:
+            try:
+                return await self.q.get_nowait()
+            except aio.QueueEmpty:
+                if self.queuer.done():
+                    raise StopAsyncIteration
+                await aio.sleep(0)
+                continue
 
 
 class AsyncMap:
-    def __init__(self, q):
-        self.tasks = set()
+    """return type of Pool.amap"""
 
-        async def keepqueuing():
-            while True:
-                task = await q.get()
-                if task is None:
-                    return
-                else:
-                    self.tasks.add(task)
-
-        self.queuer = aio.spawn(keepqueuing())
+    def __init__(self, queuer: aio.Task, tasks: t.Set[aio.Task]):
+        """queuer is the task which adds submitted tasks to the tasks set."""
+        self.queuer = queuer
+        self.tasks = tasks
 
     def __aiter__(self):
         return self
--- tests/bar.py
+++ tests/bar.py
@@ -5,23 +5,25 @@ import slowpool.aio as slowpool
 
 async def rec(n):
     await aio.sleep(random.random() * 1)
-    return 1/n
+    return 1 / n
 
 
 async def main():
 
     count = 0
     async with slowpool.Pool(10) as pool:
-        async for output in pool.amap(rec, range(1, 100)):
+        async for output in pool.amap(rec, range(1, 20)):
             print(output)
             count += 1
-
         print(count)
-            
+
         print()
 
-        for output in await pool.map(rec, range(1, 20)):
-            print(output)
+        try:
+            async for output in pool.map(rec, range(20, -1, -1)):
+                print(output)
+        except ZeroDivisionError as e:
+            print(e)
 
 
 aio.run(main())
